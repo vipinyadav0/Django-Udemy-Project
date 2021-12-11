3310976169cd0089db580ec5aaece343a1ed6159
vipinyadav0 <vyvipinyadav998@gmail.com>
2021-12-11 16:18:27 +0530

    html files added to templates dir

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 
 # Create your views here.
+##############################
+##############################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####################################
+###################################
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        from = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=Fasle)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'from':form})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for mysite project.
 
@@ -120,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT= os.path.join(BASE_DIR,'static')
 
 LOGIN_REDIRECT_URL = '/'
 
